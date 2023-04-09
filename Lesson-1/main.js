dfee5917fb271ffb3fac956f7c0182a7b789faa9
ulugbek_developer <ulugbekqodirov222002@gmail.com>
2023-04-09 16:18:09 +0500

    Type-Conversion Operators Comparison

---
+++ Lesson-1/main.js
@@ -0,0 +1,60 @@
+// let a = {num:1}
+// let b = {num:1}
+// console.log(typeof a , typeof b);
+// console.log({a} === {b});
+
+// let c = null;
+// let d = null;
+
+// console.log(typeof c , typeof d);
+// console.log(c == d);
+// console.log(false == null);
+// console.log(false  + 2);
+// console.log(true + 2);
+// console.log([null] + 1);
+
+// console.log(false + 1);
+
+
+// console.log(null  + 1);
+// console.log(false == null);
+// 
+// let  ar = [1,2,3,4,5,6];
+// console.log(ar[0]);
+// let obj = {numm:1 ,num:2,}
+// console.log(obj.numm);
+
+// let n = null 
+// console.log(n * 10);
+
+// let a = 1 
+// let b = '2'
+// // let c = a + b
+// // console.log(typeof c);
+
+// console.log(+a +  ++b);
+
+// let a = '10'
+// let b = '10.2'
+
+// console.log(Number(b));
+// console.log(parseInt(b));
+// console.log(parseFloat(b));
+
+
+// console.log(null === 0);
+// console.log(false == 0);
+// console.log(null == false);
+// console.log(true == '1');
+// console.log(true == 's');
+
+// console.log(1);
+// for( i = 0; i < 10; i ++){
+//     console.log(2);
+// }
+// console.log(3);
+
+
+//Commit
+
+
