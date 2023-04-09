1563624b1ca955cfc057cdc95e2d481753374d7b
ulugbek_developer <ulugbekqodirov222002@gmail.com>
2023-04-09 16:26:12 +0500

    If-else Nullish

---
+++ Lesson-2/main.js
@@ -0,0 +1,132 @@
+// let a = '1'
+// console.log(Number(a));
+// console.log(parseFloat(a));
+// console.log(parseInt(a));
+// console.log(+a);
+
+// var age = 20;
+
+//  age = 10
+// console.log(age);
+// console.log(age);
+
+// let age;
+// console.log(age);
+// console.log(null == undefined);
+// console.log(null === undefined);
+//single thread
+
+// console.log(1);
+
+// for(i= 0; i< 1000; i++){
+//     console.log(2);
+// }
+
+// console.log(3);
+
+//async
+// console.log(1);
+
+// setTimeout(()=>{
+// console.log(2);
+// })
+// console.log(3);
+
+// let age = null; 
+// console.log( age);
+// console.log(null == undefined);
+// console.log(1 == '1');
+
+// console.log(2**(1+2)/2);
+// console.log(3**2);
+// console.log(9/2);
+
+// console.log(1 !== "1");
+// console.log(1 != "1");
+
+// console.log(1 === "1");
+// console.log(1 == "1");
+
+// console.log(2003 || true || 'kkk' );
+// console.log((0 && true  && "hey"));
+// let temp = 12
+//  if(temp){
+//     console.log(1);  // null, false , "" , 0  , undefined
+//  }
+//  else if(temp){
+//     console.log(2);
+//  }
+//  console.log(temp);
+// console.log(null == 0);
+// let temp = null
+
+// if(temp <= 0){
+//     console.log(1);
+// }
+//  else if (temp <= 10){
+//     console.log(2);
+// }
+// else if (temp <= 15){
+//     console.log(3);
+// }
+// else if (temp <= 25){
+//     console.log(4);
+// }
+// else{
+//     console.log(5);
+// }
+
+//Nullish (??) null , undefined 
+//OR (||) null , undefined , false , "" , 0 
+
+//exponantial
+// console.log(2**50);
+
+
+        ///// HOMEWORK
+
+
+// if('0'){
+//     console.log(1);
+// }
+
+
+// let result = prompt("what is the official name of Java Script ")
+
+// if(result === "ECMAScript"){
+//     alert("Right")
+// }else{
+//     alert("You don't know ECMAScript")
+// }
+
+// let a = 2;
+// let b = 0;
+// let result ;
+
+// // if(a+b < 4){
+// //  console.log(result = "Below");   
+// // }
+// // else{
+// //  console.log(result = "Over"); 
+// // }
+
+// a + b <= 4 
+// ? console.log(result = "Below")  
+// : console.log(result = "Over")
+
+// let login = 2
+// // let login = prompt();
+// let message;
+
+//   login === "Employee"
+//  ? console.log(message = "Hello")
+//  : login === "Director" 
+//  ? console.log(message = "Greetings" )
+//  : login === ""
+//  ? console.log(message = "No login")
+//  : console.log(message = 'Sorry');
+
+
+
+
+
