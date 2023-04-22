729fb5587df4e31f2c6283e677d1df41e2840c57
ulugbek_developer <ulugbekqodirov222002@gmail.com>
2023-04-23 00:50:28 +0500

    Destructure / Date obj / JSON

---
+++ Lesson-12/main.js
@@ -0,0 +1,67 @@
+   /// JSON ///
+
+//Distructure in Object
+
+// let obj = {name:"webbrain" , title: "IT Center"}   
+// let name  = "ulugbek";
+// const { age ,name:newName ,title} = obj
+// console.log(age ,newName , title);
+
+
+
+//Distructure in Array
+
+// let array = [1, "apple" , "orange" , "Kiwi"]
+// let [one , two , three] = array
+// console.log(one);
+
+// let array = [1, "apple" , "orange" , "Kiwi"]
+// let [one , ...res] = array
+// console.log(res);
+
+
+// New Date //
+
+// let date = new Date()
+// console.log(date);
+// // console.log(Date.now());
+// // console.log(date.getTime());
+// console.log(date.getDay());
+
+// let weeks = [  "Sunday" , "Monday" , "Tuesday" , "Wednesday","Thursday" , "Friday" , "Saturday" ] ;
+
+// let date = new Date();
+// let result = weeks[date.getDay()].slice(0 , 3);
+// console.log(result);
+
+
+// JSON ///
+
+// JSON.stringify ===> Object to JSON
+
+// let user = { name: "Ulug'bek" , year: 1777 , data:{id:1 , year : 2003 , status: "dev"}}
+// let result = JSON.stringify(user ,['name', 'year', 'data' , 'id'], 4)
+// console.log(result);
+
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
