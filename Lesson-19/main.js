37b3dd476d5cb4e3abbb305a51b33686eb0c2fbb
ulugbek_developer <ulugbekqodirov222002@gmail.com>
2023-04-28 21:51:43 +0500

    Classes Basics

---
+++ Lesson-19/main.js
@@ -0,0 +1,85 @@
+// Classes //
+
+// class Users {
+//     constructor(title){
+//         this.title = title
+// // console.log(this);
+//     }
+// fullName(){
+//     console.log(`Hi "${this.title}" Weclome to Europe`);
+// }
+// }
+// let user = new Users("Webbrain")
+// let user2 = new Users("Digital One")
+
+
+
+// user.fullName();
+// user.fullName();
+
+// user2.fullName()
+// user2.fullName()
+
+
+// class Companies {
+// constructor(company){
+// this.frontend = company
+// this.backend = company
+// }
+// findNameForDigital(est){
+//     console.log(`Hi ${this.frontend} ${est}, Welcome Europe`);
+// }
+// findNameForHumans(est){
+//     console.log(`Hi ${this.backend} ${est}, Welcome Europe`);
+// }
+// }
+
+// let DigitalOne = new Companies('DigitalOne')
+// let Humans = new Companies('Humans')
+
+// DigitalOne.findNameForDigital(2003)
+// Humans.findNameForHumans(2005)
+
+
+// class Speed {
+//     constructor(name){
+//      this.rabbit = name
+//     }
+//     speed(speed){
+//         console.log(`${this.rabbit} runs ${speed}km speed per hour`);
+//     }
+    
+// }
+
+// class Animals extends Speed{
+//     constructor(name){
+//         super(name)
+//     }  
+// }
+
+
+// let rabbit = new Animals("Rabbit")
+// rabbit.speed(34)
+// let fox = new Animals("Fox")
+// fox.speed(44)
+
+
+// class Speed {
+//     constructor(name){
+//      console.log(name);
+//     }
+   
+    
+// }
+
+// class Animals extends Speed{}   //Default  this is similar with belove
+
+// class Animals extends Speed{    //Default this is similar with above 
+//     constructor(...args){
+//         super(...args)
+//     }  
+// }
+
+
+// let rabbit = new Speed("Rabbit")
+// let fox = new Animals("Fox")
