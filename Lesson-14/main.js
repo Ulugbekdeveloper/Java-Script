905a14224aacbc8508e60298f809d8727703dfec
ulugbek_developer <ulugbekqodirov222002@gmail.com>
2023-04-28 17:39:41 +0500

    IIFE and NFE Functions

---
+++ Lesson-14/main.js
@@ -0,0 +1,41 @@
+// IIFE  ===> Immediately invoked function expression
+
+// var a = 10;
+// function get (){
+//     a = 30 
+//     console.log(a);
+// }
+// get()
+// console.log(a);
+
+
+
+// IIFE ===> syntax 
+
+// var  a = 10 
+// console.log(a);
+// (function(){
+//   a = 30
+// console.log(a);
+// })()
+// console.log(a);
+
+
+
+// NFE  ===> named function expression
+
+// const getName = function recursion(name){
+//     console.log(`Hi ${name}`);
+// //   return  getName(name)
+// //   return recursion(name)
+// }
+// getName("Webbrain")
+// getName("PDP")
+// getName("NT")
+
+
+
+// new Function
+
+// let getName = new Function(console.log(1))
+// getName()
