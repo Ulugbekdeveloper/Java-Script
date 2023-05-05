647aa13f5c1f1077000e03d695afe8b37d3547d4
ulugbek_developer <ulugbekqodirov222002@gmail.com>
2023-05-05 23:22:33 +0500

    DOM-3

---
+++ Lesson-29/data.js
@@ -0,0 +1,9 @@
+// export let users = [
+//     {id:1, name:"Abdulloh1"},
+//     {id:2, name:"Abdulloh2"},
+//     {id:3, name:"Abdulloh3"},
+//     {id:4, name:"Abdulloh4"},
+//     {id:5, name:"Abdulloh5"},
+//     {id:6, name:"Abdulloh6"},
+// ]
+
---
+++ Lesson-29/main.js
@@ -0,0 +1,40 @@
+// import {users} from './data.js'
+//  function test (n){
+//    console.log(n);
+//  }
+
+// function move (move){
+//     // console.log("X",move.x , "Y" ,move.y);
+//     console.log(move);
+
+// }
+
+// button.onclick= function(){
+//     console.log("hey");
+// }
+// let data = users
+// function ondelete(id){
+//   let res = data.filter((val)=>val.id !== id )
+//   data = res
+//   container.innerHTML = null
+//   read()
+//   console.log(res);
+// }
+
+
+// function read(){
+//     if(!data.length) container.innerHTML = 'No data'
+//     data.forEach((value) => {
+//         let div = document.createElement('div')
+//         let button = document.createElement('button')
+//         button.innerHTML = 'Delete'
+//         button.addEventListener('click' , ()=>ondelete(value.id))
+//         div.innerHTML = `${value.id} - ${value.name} `
+//         container.append(button)
+//         container.append(div)  
+    
+//     })
+// }
+
+
+// read()
