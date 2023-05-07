0af38e9cce7fbe1fc0adf8145741222e48110610
ulugbek_developer <ulugbekqodirov222002@gmail.com>
2023-05-07 19:14:38 +0500

    Form

---
+++ Lesson-32/main.js
@@ -0,0 +1,37 @@
+// console.log(document.forms[2]);
+// console.log(document.forms.bunker);
+
+// console.log(document.forms.bunker.login.value);
+
+// console.log(document.forms.bunker.selection.value);
+
+// console.log(document.forms.bunker.choice);
+// console.log(document.bunker.choice.value);
+
+// let form = document.forms.bunker
+
+// login.onblur = (event)=>{
+// console.log('test' , event.target.value);
+// if(event.target.value.includes("@")){
+//     loginError.innerHTML = null;
+// }else{
+//     loginError.innerHTML = `Error`
+//     loginError.style.color = `red`
+// }
+// };
+
+// login.onkeypress = (event)=>{
+//     loginError.innerHTML = null;
+// }
+
+// login.onfocus = (event)=>{
+//     loginError.innerHTML = null;
+// }
+
+
+
+// form.onsubmit = (event) => {
+//     form.action = `https://jsonplaceholder.typicode.com/todos/${form.selection.value}`;
+    // event.preventDefault();
+//     console.log(form.selection.value);
+// }
