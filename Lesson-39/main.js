c1517cdc8ef64554d4ab3c9cdc6920c0094aa390
ulugbek_developer <ulugbekqodirov222002@gmail.com>
2023-05-22 23:02:41 +0500

    SearchParams/ URL link

---
+++ Lesson-39/main.js
@@ -0,0 +1,39 @@
+
+
+// let url = new URL(window.location)
+// let search = url.searchParams
+
+// address.value = search.get("address")
+// city.value = search.get("city")
+// zip.value = search.get("zip")
+
+// const replace = (name, value) => {
+//     if(!value){
+//         search.delete(name)
+//     }else{
+//         // search.set(name , value)
+//         search.append(name , value)
+//     }
+//     pathName.innerText = url.href
+//     return url.href
+// }
+
+// let div = document.createElement("a")
+// const changeUrl =(e) =>{
+//     const {
+//         target:{ value ,name }
+//     } = e;
+   // div.href = 
+//     replace(name, value)
+    // div.click()
+// }
+
+
+
+
+
+
+// console.log(search.delete("address"));
+// console.log(search.get("address"));
+// console.log(search.set("address" , "gunjadong"));
+// console.log(url.search);
