49924836f58fc619d405215304abb0fe9727f78a
ulugbek_developer <ulugbekqodirov222002@gmail.com>
2023-05-23 21:29:54 +0500

    Cookies

---
+++ Lesson-43/main.js
@@ -0,0 +1,6 @@
+// let date =  new Date(2023, 1,3)
+// date.toUTCString()
+// document.cookie = `name=webbrain; expire=${date}`
+// document.cookie = `name=webbrain; max-age=1; samesite=lax`
+// document.cookie = `title=just; secure; samesite=strict`
+// console.log(document.cookie);
