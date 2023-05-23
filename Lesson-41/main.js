475660c7d3acb509fb674b9696791b7310f6a04d
ulugbek_developer <ulugbekqodirov222002@gmail.com>
2023-05-23 19:42:10 +0500

    LocalStorage

---
+++ Lesson-41/main.js
@@ -0,0 +1,21 @@
+// window.onload = () => {
+// let role = localStorage.getItem("RoleID")
+
+// if(role === "admin"){
+//     user.innerText = "Admin"
+// }
+// else if (role === "driver"){
+//     user.innerText = "Driver"
+// }
+// }
+
+// save.onclick = () => {
+//     if(login.value === "webbrain"){
+//         user.innerText = "Admin"
+//         localStorage.setItem("RoleID", "admin" )
+//     }
+//     else if (login.value === "driver"){
+//         user.innerText = "Driver"
+//         localStorage.setItem("RoleID" , "driver")
+//     }
+// }
