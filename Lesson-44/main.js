6bfa26ace056fc9a0b3a312d7cf1d915f5bc0b22
ulugbek_developer <ulugbekqodirov222002@gmail.com>
2023-05-25 12:20:46 +0500

    URL/URI/SQL Injection

---
+++ Lesson-44/main.js
@@ -0,0 +1,21 @@
+//! URL /URI
+// let city = "Tashkent #64"
+// let url = new URL(`https://www.amazon.com/s?i=${city}&bbn=23485844011&rh=n%3A23485844011%2Cn%3A1040660%2Cn%3A1048188&s=featured-rank&ds=plus_gw_DCC_May_trend_denim&content-id=amzn1.sym.353f2924-e405-4b4f-bf96-d5b798e102ba%3Aamzn1.sym.353f2924-e405-4b4f-bf96-d5b798e102ba&pd_rd_r=6fb77c40-c230-4ca4-8160-0570ffa445b8&pd_rd_w=nxP9T&pd_rd_wg=O3BWl&pf_rd_p=353f2924-e405-4b4f-bf96-d5b798e102ba&pf_rd_r=DV0RXNZZE0S2PJ1BR39J&qid=1683847452&rnid=1040660&wi=leq23gy7_6&ref=sr_st_featured-rank`)
+// console.log(url);
+
+// let city = "Tashkent #64"
+// let url =  new URL(`https://rent-houze.netlify.app/properties`);
+// let search = url.searchParams;
+//?set
+// search.set("address" , city)
+//?encodeURIComponent vs decodeURIComponent
+// search.set("region" , encodeURIComponent(city))
+// search.set("region" , decodeURIComponent(city))
+//? encodeURI vs decodeURI
+// search.set("region" , encodeURI(city))
+// search.set("region" , decodeURI(city))
+// console.log(decodeURI(search.get("region")));
+
+// inp.oninput = (event) => {
+//  div.innerHTML = event.target.value
+// }
